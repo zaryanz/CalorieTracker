c8711f6e8c1cf1733b02bc0e4f970ee8567a47e1
zaryanz <aryanx404@gmail.com>
2024-02-15 23:35:23 +0530

    Initial Commit

---
+++ CalorieTracker/CalorieTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieTrackerApp.swift
+//  CalorieTracker
+//
+//  Created by Aryan Singh on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CalorieTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
